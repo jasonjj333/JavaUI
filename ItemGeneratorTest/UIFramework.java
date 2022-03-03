ba07ef8033aae5672b4cbba9516eab2694041fea
Jason John <jasonjohn333@yahoo.com>
2022-03-03 00:53:37 -0600

    commit

--- ItemGeneratorTest/UIFramework.java
+++ ItemGeneratorTest/UIFramework.java
@@ -1,21 +1,36 @@
 package ItemGeneratorTest;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
 
 public class UIFramework implements ActionListener {
     JFrame frame;
     JButton executeButton;
     JPanel custPanel;
     JPanel exPanel;
+
+    JPanel cycleNumberPanel;
+    JCheckBox cycleNumberBox;
+    JTextPane cycleNumberField;
+
+    JPanel maxItemPanel;
+    JCheckBox maxItemBox;
+    JTextPane maxItemField;
+
     public UIFramework() {
         //JFrame
         frame = new JFrame();
@@ -24,7 +39,8 @@ public class UIFramework implements ActionListener {
         frame.setFont(new Font("Arial",Font.BOLD, 20));
         frame.setBounds(500,150,450,600);
         frame.setLayout(null);
-        
+        frame.setResizable(false);
+
         //ImageIcon
         ImageIcon argoIcon = new ImageIcon("RealArgoIcon.png");
         frame.setIconImage(argoIcon.getImage());
@@ -33,8 +49,46 @@ public class UIFramework implements ActionListener {
         custPanel = new JPanel();
         custPanel.setBounds(0,0,450, 500);
         custPanel.setBackground(new Color(161, 61, 61));
-        
-        
+        custPanel.setLayout(null);
+
+        //*****CUSTOMIZATION PANEL FEATURES*****
+        //Cycle Number
+        cycleNumberPanel = new JPanel();
+        cycleNumberPanel.setBounds(0,0,200,150);
+        cycleNumberPanel.setBackground(new Color(161, 61, 61));
+
+        cycleNumberBox = new JCheckBox();
+        cycleNumberBox.setText("Cycle Number");
+        cycleNumberBox.setFocusable(false);
+        cycleNumberBox.addActionListener(this);
+        cycleNumberBox.setBounds(0,0,100,50);
+        cycleNumberBox.setForeground(Color.white);
+        cycleNumberBox.setBackground(new Color(161, 61, 61));
+
+        cycleNumberField = new JTextPane();
+        cycleNumberField.setPreferredSize(new Dimension(50,20));
+        cycleNumberField.setVisible(false);
+
+        cycleNumberPanel.add(cycleNumberBox);
+        cycleNumberPanel.add(cycleNumberField);
+
+        //Max Item
+
+        maxItemPanel = new JPanel();
+        maxItemPanel.setBounds(0,200,200,150);
+        maxItemPanel.setBackground(Color.blue);
+
+        maxItemBox = new JCheckBox();
+        maxItemBox.setText("Max Item");
+        maxItemBox.setFocusable(false);
+        maxItemBox.addActionListener(this);
+        maxItemBox.setBounds(0,0,100,50);
+        maxItemBox.setForeground(Color.white);
+        maxItemBox.setBackground(new Color(161, 61, 61));
+
+        maxItemField = new JTextPane();
+        maxItemField.setPreferredSize(new Dimension(50,20));
+        maxItemField.setVisible(false);
 
         //Execute Panel
         exPanel = new JPanel();
@@ -53,6 +107,7 @@ public class UIFramework implements ActionListener {
 
 
         //add to frame and execute
+        custPanel.add(cycleNumberPanel);
         frame.add(custPanel);
         frame.add(exPanel);
         frame.setVisible(true);
@@ -62,6 +117,16 @@ public class UIFramework implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==executeButton){
             System.out.println("Item Generator executed.");
+            if(cycleNumberBox.isSelected()) {
+                System.out.println("Cycle Number: " + cycleNumberField.getText());
+            }
+        }
+        if(e.getSource() == cycleNumberBox) {
+            if(cycleNumberBox.isSelected())
+                cycleNumberField.setVisible(true);
+            else
+                cycleNumberField.setVisible(false);
+            //frame.setVisible(true);
         }
         
     }
--- ItemGeneratorTest/UITest.java
+++ ItemGeneratorTest/UITest.java
@@ -1,6 +1,8 @@
 package ItemGeneratorTest;
 public class UITest {
     public static void main(String[] args) {
-        UIFramework run = new UIFramework();
+        //UIFramework run = new UIFramework();
+        UIFramework2 run = new UIFramework2();
+
     }
 }
